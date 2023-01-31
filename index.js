629539a1774b08669c00578a2b709c0010d30342
Agent 3 <agent.3@VENTIUS.LOCAL>
2023-01-31 11:33:25 +0100

    Style CSS done

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const path = require('path');
 const app = express();
 
 //Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/openai',require('./routes/openaiRoutes'))
+app.use('/openai',require('./routes/openaiRoutes'));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
